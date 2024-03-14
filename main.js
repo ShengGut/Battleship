0995ce2f92f5875ebbbe8d0acba8152a6142bb07
ShengGut <khy.l.jason@gmail.com>
2024-03-13 22:18:02 -0700

    feat: Wrote function to create ship object and it passes test cases

---
+++ main.js
@@ -0,0 +1,16 @@
+function createShip(length = 3, hitCount = 0) {
+  return {
+    length: length,
+    hitCount: hitCount,
+    isSunk() {
+      return this.hitCount >= this.length
+    },
+    hit() {
+      return this.hitCount++
+    },
+  }
+}
+
+module.exports = {
+  createShip,
+}
--- main.test.js
+++ main.test.js
@@ -1,5 +1,37 @@
-const {} = require('./main')
+const { createShip } = require('./main')
 
-test('Create ship (1)', () => {
-  expect(createShip()).toEqual({ length: 5, hitCount: 0, isSunk: false })
+describe('Test createShip factory function and the resulting object', () => {
+  let ship
+  const length = 3
+
+  beforeEach(() => {
+    ship = createShip(length)
+  })
+
+  test('should create an an object with the correct attributes', () => {
+    expect(ship).toHaveProperty('length', length)
+    expect(ship).toHaveProperty('hitCount', 0)
+    expect(ship).toHaveProperty('isSunk')
+    expect(ship).toHaveProperty('hit')
+  })
+
+  test('should not be be sunk initially', () => {
+    expect(ship.isSunk()).toBe(false)
+  })
+
+  test('should increment counter when hit', () => {
+    ship.hit()
+    expect(ship.hitCount).toBe(1)
+  })
+
+  test('should not be sunk until hitCount equals length', () => {
+    ship.hit()
+    expect(ship.isSunk()).toBe(false)
+
+    ship.hit()
+    expect(ship.isSunk()).toBe(false)
+
+    ship.hit()
+    expect(ship.isSunk()).toBe(true)
+  })
 })
