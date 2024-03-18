323cfdcb8b7107721021cb3f32cc89e4c49e1ceb
ShengGut <khy.l.jason@gmail.com>
2024-03-17 22:48:47 -0700

    feat: Added 10x10 grid game board

--- main.js
+++ main.js
@@ -1,3 +1,43 @@
 import './style.css'
 
-console.log('Hello world')
+// prettier-ignore
+const gameBoard = () => {
+    return [
+    '', '', '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '', '', '']
+}
+
+function handleCellClick(event) {
+  const cell = event.target
+}
+
+function renderBoard(boardElement) {
+  const boardTable = document.createElement('table')
+
+  for (let row = 0; row < 10; row++) {
+    const tableRow = document.createElement('tr')
+
+    for (let col = 0; col < 10; col++) {
+      const tableCell = document.createElement('td')
+      tableCell.addEventListener('click', handleCellClick)
+      tableRow.appendChild(tableCell)
+    }
+    boardTable.appendChild(tableRow)
+  }
+  boardElement.innerHTML = ''
+  boardElement.appendChild(boardTable)
+}
+
+// Render the game boards
+const player1Board = document.querySelector('.board.p1')
+const player2Board = document.querySelector('.board.p2')
+renderBoard(player1Board)
+renderBoard(player2Board)
